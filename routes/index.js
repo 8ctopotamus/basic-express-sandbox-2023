aa3004a3878fb9a7c69e045b2dce4e84e747efb1
Josh <zylo.codes@gmail.com>
2023-08-14 19:07:54 -0500

    routes modules cont'd

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,10 @@
 const router = require('express').Router()
+const viewRoutes = require('./views')
+const petApiRoutes = require('./pets')
+
+// api routes
+router.use('/api', petApiRoutes)
+// page/view routes
+router.use('/', viewRoutes)
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,15 +1,14 @@
+const routes = require('./routes')
 const express = require('express')
 const app = express()
 const PORT = 3000
-
-
 
 // allows the browser to request ANYTHING out of the public folder
 app.use(express.static('public'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.use()
+app.use(routes)
 
 app.listen(PORT, () => {
   console.log(`Express listening on http://localhost:${PORT}`)
